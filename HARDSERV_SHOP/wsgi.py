39fcd82b44726a1d2e8607710d77064e12b5a136
Taras <taras_ilyashchuk161@ukr.net>
2020-01-25 14:42:25 +0200

    Initial commit

---
+++ HARDSERV_SHOP/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for HARDSERV_SHOP project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'HARDSERV_SHOP.settings')
+
+application = get_wsgi_application()
