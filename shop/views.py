c112bf8453a01f1296259257f2e264ebd055e89c
Taras <taras_ilyashchuk161@ukr.net>
2020-01-30 21:11:19 +0200

    siski final version

--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,24 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from .models import Goods
+from .forms import NewGoodsForm
 # Create your views here.
 def index(request):
-    return render(request,'index.html')
+    goods = Goods.objects.all()
+    context = {'goods':goods}
+    return render(request,'index.html',context=context)
+
+def new_goods(request):
+    form = NewGoodsForm()
+    context = {'form':form}
+    if request.method == 'POST':
+        form = NewGoodsForm(request.POST, request.FILES)
+        if form.is_valid():
+            good = Goods.objects.create(name = form.cleaned_data['name'],
+                                        image=form.cleaned_data['image'],
+                                        current_price = form.cleaned_data['current_price'],
+                                        old_price = form.cleaned_data['old_price'],
+                                        info = form.cleaned_data['info']
+                                        )
+            return redirect('index')
+    return render(request,'new_goods.html',context)
+
