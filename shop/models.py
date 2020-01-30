1d1937192218d4938cb1c811cd181a213324bde6
Taras <taras_ilyashchuk161@ukr.net>
2020-01-30 21:32:50 +0200

    siski final version

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Goods(models.Model):
+    name = models.CharField(verbose_name='Name',max_length=100)
+    info = models.TextField(verbose_name='Info')
+    image = models.ImageField(upload_to='media/',blank=True, verbose_name='image')
+    current_price = models.FloatField(default=0,verbose_name='Current price')
+    old_price = models.FloatField(default=0,verbose_name='Old Price')
+    class Meta:
+        pass
