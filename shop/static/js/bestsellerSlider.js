99bbcfd9566fba7bacf42925cc167d94ffab6b50
Dmytro <vayren0965@gmail.com>
2020-02-01 21:31:23 +0200

    bestsellers

---
+++ shop/static/js/bestsellerSlider.js
@@ -0,0 +1,26 @@
+(function () {
+	function bestsellerSlider(selector, width, count) {
+		count--;
+		let List = document.querySelector(`.${selector} ul`);
+		let countList = 0, position = 0;
+
+		document.querySelector(`.${selector} .arrow-right`).addEventListener('click', clickRight);
+		document.querySelector(`.${selector} .arrow-left`).addEventListener('click', clickLeft);
+
+		function clickRight(){
+			if(countList < count){
+				position += -1 * width;
+				List.style.marginLeft = position + 'px';
+				countList++;
+			}
+		}
+		function clickLeft() {
+			if(countList > 0){
+				position += width;
+				List.style.marginLeft = position + 'px';
+				countList--;
+			}
+		}
+	}
+	bestsellerSlider('bestsellers', 262, 2);
+})();
